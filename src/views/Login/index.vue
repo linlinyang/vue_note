b61507520c1ab969f08aa7cd43c261b62ae472dc
linlinyang <935704186@qq.com>
2019-07-28 13:14:24 +0800

    vue cli css module bug

---
+++ src/views/Login/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div :class="[styles.button,styles.global_button]">
+        <span>按钮</span>
+    </div>
+</template>
+
+<script>
+import styles from './index.less';
+console.log(styles);
+export default {
+    name: 'Login',
+    data(){
+        return {
+            styles
+        };
+    }
+}
+</script>
+
+
